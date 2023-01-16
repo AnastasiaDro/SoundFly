af1c8dc52891faee74bb7c64d21224b04cb15c57
Anastasia Drogunova <a.drogunova@start.ru>
2023-01-17 00:40:21 +0400

    added asking permission for audio recording

--- app/src/main/java/com/soundapp/SoundFly/MainActivity.kt
+++ app/src/main/java/com/soundapp/SoundFly/MainActivity.kt
@@ -1,11 +1,58 @@
 package com.soundapp.SoundFly
 
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.google.android.material.snackbar.Snackbar
+import com.soundapp.SoundFly.databinding.ActivityMainBinding
+import java.util.jar.Manifest
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var viewBinding: ActivityMainBinding
+
+    private var permissions = arrayOf(android.Manifest.permission.RECORD_AUDIO)
+    private var permissionGranted = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        viewBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(viewBinding.root)
+
+        viewBinding.btnRecord.setOnClickListener {
+            startRecording()
+        }
     }
+
+    private fun startRecording() {
+        if (checkMicrophoneAppearance()) requestPermission() else Toast.makeText(applicationContext, R.string.no_micro, Toast.LENGTH_SHORT).show()
+
+    }
+
+    private fun checkMicrophoneAppearance(): Boolean {
+        return (this.packageManager.hasSystemFeature(PackageManager.FEATURE_MICROPHONE))
+    }
+
+    private fun requestPermission() {
+        when {
+            ContextCompat.checkSelfPermission(this, android.Manifest.permission.RECORD_AUDIO) == PackageManager.PERMISSION_GRANTED -> {
+                Log.i("Permission", "Granted")
+            }
+            ActivityCompat.shouldShowRequestPermissionRationale(this, "RECORD_AUDIO") -> {
+                requestPermissionLauncher.launch(android.Manifest.permission.RECORD_AUDIO)
+            }
+            else -> { requestPermissionLauncher.launch(android.Manifest.permission.RECORD_AUDIO) }
+        }
+    }
+
+    private val requestPermissionLauncher =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted: Boolean ->
+            if (isGranted) { Log.i("Permission", "Granted") }
+            else { Log.i("permission", "Denied") }
+        }
 }
